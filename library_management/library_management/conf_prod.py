73ba3bfffd454a5d73a6908ecb766fa1ce7a4a56
veneberg <grebenev-81@mail.ru>
2023-11-29 17:42:03 +0300

    edit conf_prod

--- library_management/library_management/conf_prod.py
+++ library_management/library_management/conf_prod.py
@@ -7,15 +7,18 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 DEBUG = False
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "braniac",
-        "USER": "django",
-        "PASSWORD": "geekbrains",
-        "HOST": "127.0.0.1",
-        "PORT": "5432",
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'your_mysql_database_name',
+        'USER': 'your_mysql_username',
+        'PASSWORD': 'your_mysql_password',
+        'HOST': 'db',
+        'PORT': '3306',
     }
 }
 
 del STATICFILES_DIRS
 STATIC_ROOT = BASE_DIR / "static"
+
+CELERY_BROKER_URL = 'redis://redis:6379/0'
+CELERY_RESULT_BACKEND = 'redis://redis:6379/0'
